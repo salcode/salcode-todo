b1237eebaa977ab0f07004e36a8f5d016de1368b
Sal Ferrarello <sal@ironcodestudio.com>
2021-02-21 11:27:53 -0500

    Add stub list items

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ function App() {
       <header className="App-header">
         <h1>salcode Todo</h1>
       </header>
+      <ul className="todo-list">
+        <li>item 1</li>
+        <li>item 2</li>
+        <li>item 3</li>
+      </ul>
     </div>
   );
 }
